c7869e864ce9b4e52e0f5b07548d880b4498940f
emersonpisoni <emersonpisoni14@gmail.com>
2023-10-24 16:10:48 -0300

    change manifest

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === 'development',
+  skipWaiting: true,
 })
 
 module.exports = withPWA({
   // next.js config
-  output: 'export'
+  // pwa: {
+
+  // },
+  // output: 'export'
 })
